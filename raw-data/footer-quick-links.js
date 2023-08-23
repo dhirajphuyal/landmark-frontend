d6900b5eb711cee4351fd741f59ef7004c499329
praful thapa <praful@waterflowtechnology.net>
2023-08-23 17:08:03 +0545

    feat: footer component

---
+++ raw-data/footer-quick-links.js
@@ -0,0 +1,116 @@
+export const usefulLinks = [
+  {
+    name: "Securities Board of Nepal",
+    link: "https://www.sebon.gov.np/",
+  },
+  {
+    name: "Nepal Stock Exchange",
+    link: "https://www.nepalstock.com/",
+  },
+  {
+    name: "CDS and Clearing Limited",
+    link: "https://cdsc.com.np/",
+  },
+  {
+    name: "Merolagani",
+    link: "https://merolagani.com/",
+  },
+  {
+    name: "UN Consolidated Sanction List",
+    link: "https://www.un.org/securitycouncil/content/un-sc-consolidated-list",
+  },
+];
+
+export const quickLinks = [
+  {
+    name: "Open an Account",
+    link: "https://kyc.naasasecurities.com.np/Users/PersonalInfo",
+  },
+
+  {
+    name: "TMS 58 Login",
+    link: "https://tms58.nepsetms.com.np/login",
+  },
+  {
+    name: "Meroshare Login",
+    link: "https://meroshare.cdsc.com.np/#/login",
+  },
+];
+
+export const companyLinks = [
+  {
+    name: "About Us",
+    link: "/about",
+  },
+  {
+    name: "Brokerage Charges",
+    link: "/#",
+  },
+  {
+    name: "FAQ's",
+    link: "/faqs",
+  },
+  {
+    name: "Careers",
+    link: "/careers",
+  },
+  {
+    name: "Pricing",
+    link: "/pricing/1",
+  },
+  // {
+  //   name: "Refer & Earn",
+  //   link: "/#",
+  // },
+  // {
+  //   name: "Press Releases",
+  //   link: "/#",
+  // },
+  // {
+  //   name: "Bug Bounty Program",
+  //   link: "/#",
+  // },
+  // {
+  //   name: "Afilliate Program",
+  //   link: "/#",
+  // },
+  {
+    name: "Contact Us",
+    link: "/contact-us",
+  },
+  // {
+  //   name: "Help Center",
+  //   link: "/#",
+  // },
+];
+
+export const socialsLink = [
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com/NEPSE.58/",
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/naasa_securities_co_ltd/",
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/naasasecurities",
+  },
+  // {
+  //   name: "Telegram",
+  //   link: "/#",
+  // },
+  {
+    name: "Linked in",
+    link: "https://np.linkedin.com/company/naasa-securities-company-limited",
+  },
+  {
+    name: "YouTube",
+    link: "https://www.youtube.com/channel/UCBq05llR9eO_ggWnK6_suvw",
+  },
+  // {
+  //   name: "Skype",
+  //   link: "/#",
+  // },
+];

9fb0c3c267fee01a5e03ecc4bdbff77fdfeab951
praful thapa <praful@waterflowtechnology.net>
2023-08-30 16:37:15 +0545

    feat: individual lessons

--- src/app/courses/[courseId]/page.tsx
+++ src/app/courses/[courseId]/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { AccessTime, MenuBook } from "@mui/icons-material";
-import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { useRouter, useParams } from "next/navigation";
+
+import { AccessTime, MenuBook } from "@mui/icons-material";
 import { dummyLessons } from "../../../../raw-data/dummyCourses";
 import { instance } from "../../../../config/axios";
 import LoadingSkeleton from "./loadingSkeleton";
@@ -13,11 +15,29 @@ interface GetCourseDetails {
   error: any;
 }
 
+interface IndividualCourse {
+  // course_id:string,
+  // id:string,
+  lessonsCount: number;
+  lessons: any[];
+  title: string;
+  duration: string;
+}
+
 const IndividualCoursePage = () => {
+  const params = useParams();
+  const router = useRouter();
   const [getCourse, setGetCourse] = useState<GetCourseDetails>({
     isLoading: false,
     data: null,
     error: null,
+  });
+
+  const [individualCourse, setIndividualCourse] = useState<IndividualCourse>({
+    lessonsCount: 0,
+    lessons: [],
+    title: "",
+    duration: "",
   });
 
   const getCourseDetails = async () => {
@@ -30,7 +50,6 @@ const IndividualCoursePage = () => {
         courseId: 94,
       });
       if (res) {
-        console.log(res?.data);
         setGetCourse({
           ...getCourse,
           data: res?.data,
@@ -60,6 +79,20 @@ const IndividualCoursePage = () => {
   useEffect(() => {
     getCourseDetails();
   }, []);
+
+  useEffect(() => {
+    if (getCourse.data) {
+      const individualCourse = getCourse?.data?.sections?.find(
+        (course: any) => course.id === params.courseId
+      );
+      setIndividualCourse({
+        lessonsCount: individualCourse?.lesson_counter_ends,
+        lessons: individualCourse?.lessons,
+        title: individualCourse?.title,
+        duration: individualCourse?.total_duration,
+      });
+    }
+  }, [getCourse.data]);
 
   console.log(getCourse);
 
@@ -93,16 +126,20 @@ const IndividualCoursePage = () => {
             </div>
             <div className="flex flex-col w-[60%] gap-7">
               <span className="text-2xl font-bold">
-                Introduction to Stock Market
+                {individualCourse.title}
               </span>
               <div className="flex justify-between w-[100%]">
                 <div className="flex items-center gap-2">
                   <AccessTime style={{ color: "gray" }} />
-                  <span className="font-thin text-sm">7h 0m</span>
+                  <span className="font-thin text-sm">
+                    {individualCourse?.duration}
+                  </span>
                 </div>
                 <div className="flex items-center gap-2">
                   <MenuBook style={{ color: "gray" }} />
-                  <span className="font-thin text-sm">40 Lessons</span>
+                  <span className="font-thin text-sm">
+                    {individualCourse?.lessonsCount} Lessons
+                  </span>
                 </div>
               </div>
               <span className="text-sm ">
@@ -112,13 +149,16 @@ const IndividualCoursePage = () => {
             </div>
           </div>
           <div className="flex flex-wrap">
-            {dummyLessons.map((lesson, index) => (
+            {individualCourse?.lessons?.map((lesson, index) => (
               <div
                 key={index}
                 className="w-1/3 flex flex-col items-center p-2 cursor-pointer hover:shadow-lg"
+                onClick={() =>
+                  router.push(`/courses/${params.courseId}/${lesson.id}`)
+                }
               >
                 <Image
-                  src={lesson.image}
+                  src={"/images/lessons/lesson1.png"}
                   width={300}
                   height={200}
                   style={{ height: 200, width: "100%" }}
@@ -126,10 +166,10 @@ const IndividualCoursePage = () => {
                 />
                 <div className="flex flex-col justify-between w-[100%] gap-2 p-2 ">
                   <div className="flex items-center justify-between w-[100%]">
-                    <span className="text-lg font-bold">{lesson.title}</span>
+                    <span className="text-base font-bold">{lesson.title}</span>
                     <div className="flex gap-2 items-center">
                       <AccessTime style={{ color: "gray" }} />
-                      <span className="font-thin text-sx">
+                      <span className="font-thin text-xs">
                         {lesson.duration}
                       </span>
                     </div>
